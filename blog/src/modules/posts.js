be21a8361c55c265aee63c922b7372b7baec2d17
OseungKwon <00osos@naver.com>
2021-07-25 04:24:01 +0900

    Update posts.js

--- blog/src/modules/posts.js
+++ blog/src/modules/posts.js
@@ -4,3 +4,19 @@ import createRequestSaga, {
 } from "../lib/createRequestSaga";
 import * as postAPI from '../lib/api/posts'
 import { takeLatest } from "redux-saga/effects";
+
+const [
+    LIST_POSTS,
+    LIST_POSTS_SUCCESS,
+    LIST_POSTS_FAILURE,
+] = createRequestActionTyles('posts/LIST_POSTS');
+
+export const listPosts = createAction(
+    LIST_POSTS,
+    ({ tag, username, page }) => ({ tag, username, page }),
+)
+
+const listPostsSaga = createRequestSaga(LIST_POSTS, postAPI.listPosts);
+export function* postSaga() {
+    yield takeLatest(LIST_POSTS, listPostsSaga)
+}
