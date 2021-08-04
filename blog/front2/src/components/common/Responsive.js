2dc54063d3df3f4067d194bb235bceb672528975
OseungKwon <00osos@naver.com>
2021-08-05 03:24:25 +0900

    Create Responsive.js
    반응형 웹을 위해서 media 추가

---
+++ blog/front2/src/components/common/Responsive.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ResponseBlock = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  width: 1024px;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%auto;
+  }
+`;
+const Responsive = ({ children, ...rest }) => {
+  // ...rest는 styled, classname, onClick, onMouseMove 등의 props를 사용할 수 있도록
+  // ResponseBlock에게 전달하는 역할을 한다.
+  return <ResponseBlock {...rest}>{children}</ResponseBlock>;
+};
+
+export default Responsive;
