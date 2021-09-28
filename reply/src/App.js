132af981c2177bd8b3a751a8c67a9c790e95b8b5
OseungKwon <54261116+OseungKwon@users.noreply.github.com>
2021-09-28 14:27:48 +0900

    Add toast-editor ui

--- reply/src/App.js
+++ reply/src/App.js
@@ -1,18 +1,28 @@
-import { Button } from '@mui/material'
-import React from 'react'
-import Comment from './comment/Comment'
+import { Button } from "@mui/material";
+import React from "react";
+import Comment from "./comment/Comment";
+import Toast from "./comment/Toast";
 
 const App = () => {
   return (
     <div>
-      <Button onClick={() => {
-        localStorage.clear()
-        window.location.reload()
-      }
-      }>데이터 삭제</Button>
-      <Comment />
+      <div>
+        <Toast />
+      </div>
+      <hr />
+      <div>
+        <Button
+          onClick={() => {
+            localStorage.clear();
+            window.location.reload();
+          }}
+        >
+          데이터 삭제
+        </Button>
+        <Comment />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
